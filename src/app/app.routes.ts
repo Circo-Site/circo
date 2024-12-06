8e5509ef712b8d89173dd62428bda6f1c5414f21
Drakemaner <guilhermejenner10@gmail.com>
2024-12-06 08:00:49 -0300

    Volta do /home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,11 @@ import {ConteudoComponent} from "./conteudo/conteudo.component";
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
         loadComponent: () => import('./home/home.component').then(c => c.HomeComponent),
         resolve: {
             diretores: DiretorResolver
